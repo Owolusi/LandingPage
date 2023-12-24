e57beb6fec5a318532218247da7230f91057f5cf
Owolusi Lucky <109273986+Owolusi@users.noreply.github.com>
2023-12-24 03:20:24 +0100

    CSS UPDATE3

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import gpt3Logo from '../../assets/logo.svg';
-import './footer.css';
+import './Footer.css';
 
 const Footer = () => (
   <div className="gpt3__footer section__padding">
